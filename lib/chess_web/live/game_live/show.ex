3b38eaf64df2e35a254f0f0665fc59afff98e15d
AceAz <awadziaaron@gmail.com>
2024-11-01 11:31:31 -0400

    tool tips

--- lib/chess_web/live/game_live/show.ex
+++ lib/chess_web/live/game_live/show.ex
@@ -5,18 +5,18 @@ defmodule ChessWeb.GameLive.Show do
   alias Chess.Game.GameManager
 
   @pieces %{
-    "wP" => %{symbol: "\u2659", class: "white-piece"},
-    "wR" => %{symbol: "\u2656", class: "white-piece"},
-    "wN" => %{symbol: "\u2658", class: "white-piece"},
-    "wB" => %{symbol: "\u2657", class: "white-piece"},
-    "wQ" => %{symbol: "\u2655", class: "white-piece"},
-    "wK" => %{symbol: "\u2654", class: "white-piece"},
-    "bP" => %{symbol: "\u265F", class: "black-piece"},
-    "bR" => %{symbol: "\u265C", class: "black-piece"},
-    "bN" => %{symbol: "\u265E", class: "black-piece"},
-    "bB" => %{symbol: "\u265D", class: "black-piece"},
-    "bQ" => %{symbol: "\u265B", class: "black-piece"},
-    "bK" => %{symbol: "\u265A", class: "black-piece"}
+    "wP" => %{symbol: "\u2659", class: "white-piece", description: "Pawn: Moves forward one square, attacks diagonally."},
+    "wR" => %{symbol: "\u2656", class: "white-piece", description: "Rook: Moves horizontally or vertically any number of squares."},
+    "wN" => %{symbol: "\u2658", class: "white-piece", description: "Knight: Moves in an 'L' shape, can jump over pieces."},
+    "wB" => %{symbol: "\u2657", class: "white-piece", description: "Bishop: Moves diagonally any number of squares."},
+    "wQ" => %{symbol: "\u2655", class: "white-piece", description: "Queen: Moves in any direction any number of squares."},
+    "wK" => %{symbol: "\u2654", class: "white-piece", description: "King: Moves one square in any direction. Checkmate to win!"},
+    "bP" => %{symbol: "\u265F", class: "black-piece", description: "Pawn: Moves forward one square, attacks diagonally."},
+    "bR" => %{symbol: "\u265C", class: "black-piece", description: "Rook: Moves horizontally or vertically any number of squares."},
+    "bN" => %{symbol: "\u265E", class: "black-piece", description: "Knight: Moves in an 'L' shape, can jump over pieces."},
+    "bB" => %{symbol: "\u265D", class: "black-piece", description: "Bishop: Moves diagonally any number of squares."},
+    "bQ" => %{symbol: "\u265B", class: "black-piece", description: "Queen: Moves in any direction any number of squares."},
+    "bK" => %{symbol: "\u265A", class: "black-piece", description: "King: Moves one square in any direction. Checkmate to win!"}
   }
 
   @impl true
@@ -96,13 +96,16 @@ defmodule ChessWeb.GameLive.Show do
                 piece_data = if piece, do: @pieces[piece]
               %>
               <div class={"square #{square_color} #{if is_selected, do: "selected"} #{if is_valid_move, do: "valid-move"}"}
-                   phx-click="select_square"
-                   phx-value-row={row}
-                   phx-value-col={col}>
+                  phx-click="select_square"
+                  phx-value-row={row}
+                  phx-value-col={col}>
                 <%= if piece_data do %>
                   <span class={"chess-piece #{piece_data.class}"}>
                     <%= piece_data.symbol %>
                   </span>
+                  <div class="tooltip">
+                    <%= piece_data.description %>
+                  </div>
                 <% end %>
               </div>
             <% end %>
@@ -115,6 +118,7 @@ defmodule ChessWeb.GameLive.Show do
     </div>
     """
   end
+
 
   @impl true
   def handle_event("select_square", %{"row" => row, "col" => col}, socket) do
